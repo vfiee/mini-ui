ce7e155e51017c21b4f8e4c4615f3f7cd59aade5
vyron <fiee_vyron@163.com>
2020-11-03 14:09:40 +0800

    docs(Transition): 增加Transition组件文档

--- example/src/pages/transition/index.tsx
+++ example/src/pages/transition/index.tsx
@@ -1,22 +1,92 @@
 import React, { Component } from "react";
-import { View, Text } from "@tarojs/components";
+import { View } from "@tarojs/components";
+import { Transition } from "@vyron/mini-ui";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { TransitionType } from "@vyron/mini-ui/types";
 import "./index.less";
 
-export default class Transition extends Component {
-  componentWillMount() {}
+const TRANSITION_TYPES: TransitionType[] = [
+  "fade",
+  "fadeUp",
+  "fadeDown",
+  "fadeLeft",
+  "fadeRight",
+  "slideUp",
+  "slideDown",
+  "slideLeft",
+  "slideRight",
+];
 
-  componentDidMount() {}
-
-  componentWillUnmount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-
+export default class TransitionExample extends Component {
+  state = {
+    props: {
+      show: false,
+      duration: 300,
+      name: TRANSITION_TYPES[0],
+    },
+  };
+  beforeEnter() {
+    console.log(`beforeEnter`);
+  }
+  onEnter() {
+    console.log(`onEnter`);
+  }
+  afterEnter() {
+    console.log(`afterEnter`);
+  }
+  beforeLeave() {
+    console.log(`beforeLeave`);
+  }
+  onLeave() {
+    console.log(`onLeave`);
+  }
+  afterLeave() {
+    console.log(`afterLeave`);
+  }
   render() {
     return (
       <View className="transition">
-        <Text>Hello world!</Text>
+        {TRANSITION_TYPES.map((type) => (
+          <View
+            key={type}
+            className="transition-item"
+            onClick={() => {
+              this.setState({
+                props: {
+                  ...this.state.props,
+                  name: type,
+                  show: true,
+                },
+              });
+            }}
+          >
+            {type}
+          </View>
+        ))}
+        <Transition
+          {...this.state.props}
+          beforeEnter={this.beforeEnter}
+          onEnter={this.onEnter}
+          afterEnter={this.afterEnter}
+          beforeLeave={this.beforeLeave}
+          onLeave={this.onLeave}
+          afterLeave={this.afterLeave}
+        >
+          <View
+            className="transition-child"
+            onClick={(eve) => {
+              eve.stopPropagation();
+              this.setState({
+                props: {
+                  ...this.state.props,
+                  show: false,
+                },
+              });
+            }}
+          >
+            {this.state.props.name}
+          </View>
+        </Transition>
       </View>
     );
   }
